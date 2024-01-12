28a8aea552f50ab447ad617b238636afb5237c6a
cdomingues <cris.domingues@gmail.com>
2024-01-12 20:58:04 -0300

    Primeira alua de Javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var nome = "Cristiane Domingues";
+var n1 = 42;
+var n2 = 10;
+var frase = " Eu vou ser uma ótima programadora";
+//alert(nome + " tem " + idade);
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+alert(frase.replace("Eu vou ser", "Cris é"));
